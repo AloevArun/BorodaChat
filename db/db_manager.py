30ffc001a041ae7b226ec5d5b57388a9e19f4b3e
Bananator <aloev.arun@gmail.com>
2021-07-26 22:55:33 +0300

    Реализовано добавление и удаление пользователей в БД

--- db/db_manager.py
+++ db/db_manager.py
@@ -9,6 +9,13 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'users'
+    _id = Column(Integer, primary_key=True)
+    user = Column(String(250), nullable=False)
+    password = Column(Text, nullable=False)
+
+
 class Message(Base):
     __tablename__ = 'message'
     _id = Column(Integer, primary_key=True)
@@ -23,17 +30,52 @@ class DBManager:
         self._create_session()
 
     def _create_session(self):
-        table_path = 'db/message_table.db'
-        engine = create_engine(f'sqlite:///{table_path}?check_same_thread=False')
+        database_path = 'db/main.db'
+        engine = create_engine(f'sqlite:///{database_path}?check_same_thread=False')
 
-        if not os.path.exists(table_path):
+        if not os.path.exists(database_path):
             Base.metadata.create_all(engine)
         else:
             Base.metadata.bind = engine
 
         self.session = sessionmaker(bind=engine)()
 
-    def add(self, user: str, text: str):
+    def authentification(self, user: str, password: str):
+        user = self.session.query(User).filter_by(user=user).one()
+        if user['password'] == password:
+            auth_status = 'granted'
+        else:
+            auth_status = 'denied'
+        return auth_status
+
+    def user_exists(self, user: str):
+        user_exists = False
+        for users in self.session.query(User).all():
+            usr = {'user': users.user}
+            if user == usr:
+                user_exists = True
+            else:
+                user_exists = False
+        return user_exists
+
+    def add_new_user(self, user: str, password: str):
+        if not self.user_exists(user):
+            _id = len(self.session.query(User).all()) + 1
+            user = User(_id=_id, user=user, password=password)
+            self.session.add(user)
+            self.session.commit()
+            self.session.flush()
+            return True
+        else:
+            return False
+
+    def delete_user(self, user: str):
+        user_ = self.session.query(User).filter_by(user=user).first()
+        self.session.delete(user_)
+        self.session.commit()
+        self.session.flush()
+
+    def add_new_message(self, user: str, text: str):
         _id = len(self.session.query(Message).all()) + 1
         date = arrow.now().format('YYYY-MM-DDTh:m:s.SS')
         msg = Message(_id=_id, user=user, message=text, date=date)
@@ -41,7 +83,7 @@ class DBManager:
         self.session.commit()
         self.session.flush()
 
-    def read_all(self):
+    def read_all_messages(self):
         all_messages = []
         for msg in self.session.query(Message).all():
             message = {
@@ -53,7 +95,7 @@ class DBManager:
         self.session.flush()
         return all_messages
 
-    def read_new(self, time: str):
+    def read_new_messages(self, time: str):
         new_messages = []
         for msg in self.session.query(Message).all():
             message = {
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
+
 from db.db_manager import DBManager
+
 app = Flask(__name__)
 db = DBManager()
 
@@ -9,13 +11,39 @@ def add_message():
     body = request.get_json()
     user = body['user']
     text = body['text']
-    db.add(user, text)
+    db.add_new_message(user, text)
+    return body
+
+
+@app.route('/user', methods=['POST'])
+def login():
+    body = request.get_json()
+    user = body['user']
+    password = body['password']
+    answer = db.authentification(user, password)
+    return answer
+
+
+@app.route('/registration', methods=['POST'])
+def register():
+    body = request.get_json()
+    user = body['user']
+    password = body['password']
+    db.add_new_user(user, password)
+    return body
+
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+    body = request.get_json()
+    user = body['user']
+    db.delete_user(user)
     return body
 
 
 @app.route('/msgs')
 def all_messages():
-    messages = db.read_all()
+    messages = db.read_all_messages()
     return {'messages': messages}
 
 
@@ -23,7 +51,7 @@ def all_messages():
 def detect_new_messages():
     body = request.get_json()
     time = body['time']
-    messages = db.read_new(time)
+    messages = db.read_new_messages(time)
     return {'messages': messages}
 
 
@@ -34,4 +62,3 @@ def is_online():
 
 if __name__ == '__main__':
     app.run()
-
