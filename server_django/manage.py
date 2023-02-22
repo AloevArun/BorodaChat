dc7ef1fb838aa105255398f63fac34d5f5447c05
AloevArun <aloev.arun@gmail.com>
2023-02-22 08:12:31 +0300

    github migration ok

--- server_django/manage.py
+++ server_django/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Runun administrative tasks."""
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server_django.settings')
     try:
         from django.core.management import execute_from_command_line
