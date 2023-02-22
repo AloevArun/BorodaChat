cff328d45213b820beedf49d45f61acfe3e29a81
AloevArun <aloev.arun@gmail.com>
2023-02-22 07:39:55 +0300

    django init

--- tests.py
+++ tests.py
@@ -1,18 +1,1 @@
-def max_elem1(a, b):
-    return True if a > b else False
-
-
-def max_elem2(a, b):
-    if a > b:
-        return True
-    else:
-        return False
-
-
-def max_elem3(a, b):
-    if a > b:
-        return
-
-
-
-
+print(bool(1), bool(0), sep="\n")
